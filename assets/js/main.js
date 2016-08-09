6d1510a7d1660b230f1c709da2befc83d903cfd4
Mark Swedberg <Mark Swedberg>
2016-08-09 10:44:23 -0700

    JS and CSS cleanups

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,7 +41,7 @@ $(document).ready(function() {
 
 
 				// Filter Methods
-				toggleFilters();
-				toggleCheckBoxes();
-				filterItems();
+				/*toggleFilters();
+							toggleCheckBoxes();
+							filterItems();*/
 });
