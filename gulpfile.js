1b96ee6761609eb0174eb1fd3aa97ec48c20ebfb
Mark Swedberg <Mark Swedberg>
2016-08-20 15:34:44 -0700

    Added code optimization to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,53 @@
 var gulp								= require('gulp');
-var browserSync = require('browser-sync');
-var flatten					= require('gulp-flatten');
-var stylus						= require('gulp-stylus');
-var image       = require('gulp-image');
-var prefix						= require('gulp-autoprefixer');
 var pug								 = require('gulp-pug');
-var child							= require('child_process');
+var flatten					= require('gulp-flatten');
+var htmlMin     = require('gulp-htmlmin');
+var stylus						= require('gulp-stylus');
+var prefix						= require('gulp-autoprefixer');
+var cssMin      = require('gulp-clean-css');
+var concat      = require('gulp-concat');
+var uglify      = require('gulp-uglify');
+var image       = require('gulp-image');
 var del									= require('del');
 var runSequence = require('run-sequence');
-
-var message = {
-				jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
-};
+var browserSync = require('browser-sync');
+var child							= require('child_process');
 
 
-// COMPILATION
-gulp.task('compile-html', function() {
+// COMPILE TASKS
+gulp.task('html', function() {
 				return gulp.src('_source/**/*.pug')
 															.pipe(pug())
 															.pipe(flatten())
+															.pipe(htmlMin())
 															.pipe(gulp.dest('_includes'));
 });
 
-gulp.task('compile-css', function() {
+gulp.task('css', function() {
 				return gulp.src('_source/main.styl')
 															.pipe(stylus({
 																			includePaths: ['css'],
 																			onError: browserSync.notify
 															}))
 															.pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
-															.pipe(gulp.dest('assets/css'));
-});
-
-
-// OPTIMIZATION
-// Must run the image task separately (to save load time)
-gulp.task('optimize-img', function() {
-				del('assets/images');
-				return gulp.src('_source/images/**')
-															.pipe(image({
-																			pngquant: true,
-																			optipng: true,
-																			zopflipng: true
-															}))
-															.pipe(gulp.dest('assets/images'));
-});
-
-
-// RELOCATION
-gulp.task('move-css', function() {
-				return gulp.src('assets/css/main.css')
+															.pipe(cssMin())
 															.pipe(gulp.dest('_site/assets/css'))
 															.pipe(browserSync.reload({stream:true}));
 });
 
-gulp.task('move-js', function() {
-				return gulp.src('assets/js/**')
+gulp.task('js', function() {
+				return gulp.src('assets/js/*')
+															.pipe(concat('main.js'))
+															.pipe(uglify())
 															.pipe(gulp.dest('_site/assets/js'))
 															.pipe(browserSync.reload({stream:true}));
 });
 
 
-// BUILD
+// BUILD TASKS
 gulp.task('jekyll-build', function(done) {
-				browserSync.notify(message.jekyllBuild);
 				return child.spawn('jekyll.bat', ['build'], {stdio: 'inherit'})
 																.on('close', done);
-});
-
-gulp.task('reload', ['jekyll-build'], function() {
-				browserSync.reload();
 });
 
 gulp.task('serve', function(callback) {
@@ -81,29 +59,47 @@ gulp.task('serve', function(callback) {
 				});
 });
 
-
-// WATCH
-gulp.task('watch', function() {
-				gulp.watch('_source/**/*.pug', ['compile-html']);
-				gulp.watch(['index.html', '_layouts/*.html', '_includes/*'], ['reload']);
-				gulp.watch('_source/**/*.styl', ['compile-css']);
-				gulp.watch('assets/css/**', ['move-css']);
-				gulp.watch('assets/js/**', ['move-js']);
+gulp.task('reload', ['jekyll-build'], function() {
+				browserSync.reload();
 });
 
 
-// RUN
+// RUN TASKS
+// Optimize
+gulp.task('img', function() {
+				return gulp.src('assets/images/**')
+															.pipe(image({
+																			pngquant: true,
+																			optipng: true,
+																			zopflipng: true
+															}))
+															.pipe(gulp.dest('assets/images'));
+});
+
+// Run Variable
 gulp.task('run', function(callback) {
 				runSequence(
-								'compile-html',
-								'compile-css',
-								'move-css',
-								'move-js',
-								'jekyll-build',
-								'serve',
+								'html',
+								'css',
+								'js',
 								callback
 				);
 });
 
-// Type "gulp" into the terminal to launch gulp.
-gulp.task('default', ['run', 'watch']);
+// Development
+gulp.task('default', function() {
+				runSequence(
+								'run',
+								'jekyll-build',
+								'serve',
+								'watch'
+				);
+});
+
+
+gulp.task('watch', function() {
+				gulp.watch('_source/**/*.pug', ['html']);
+				gulp.watch(['index.html', '_layouts/*.html', '_includes/*'], ['reload']);
+				gulp.watch('_source/**/*.styl', ['css']);
+				gulp.watch('assets/js/**', ['js']);
+});
